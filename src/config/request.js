5e42112eedf12822bce4f0d1dc1d14758e9ce003
Lutfullo1 <ismoilovlutfullo1117@gmail.com>
2025-06-13 00:43:29 +0500

    request, favicon

--- src/config/request.js
+++ src/config/request.js
@@ -1,31 +1,31 @@
-import axios from "axios"
-import Cookies from "js-cookie"
+import axios from "axios";
+import Cookies from "js-cookie";
 
 const request = axios.create({
-  baseURL: "https://sahifam.uz",
-})
+  baseURL: "https://api.sahifam.uz",
+});
 
 request.interceptors.request.use(
   (config) => {
-    config.headers["Authorization"] = `Bearer ${Cookies.get("user_token")}`
-    return config
+    config.headers["Authorization"] = `Bearer ${Cookies.get("user_token")}`;
+    return config;
   },
   (error) => {
-    console.error("Request interceptor error:", error)
-    return Promise.reject(error)
-  },
-)
+    console.error("Request interceptor error:", error);
+    return Promise.reject(error);
+  }
+);
 
 request.interceptors.response.use(
   (response) => {
-    return response
+    return response;
   },
   (error) => {
     if (error.response?.status === 401) {
-      window.location.href = "/"
+      window.location.href = "/";
     }
-    return Promise.reject(error)
-  },
-)
+    return Promise.reject(error);
+  }
+);
 
-export { request }
+export { request };
